fcb468e0868e8022bdd68c85d660e6fd71dfdca8
xgdwq <xgdwq@126.com>
2016-09-29 16:55:09 +0800

    commit 1.py

---
+++ 1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+########################################################################
+# 
+# Copyright (c) 2016 Baidu.com, Inc. All Rights Reserved
+# 
+########################################################################
+ 
+"""
+File: 1.py
+Author: wangqiang18(wangqiang18@baidu.com)
+Date: 2016/09/29 16:50:18
+"""
+
