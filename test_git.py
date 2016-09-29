3b2661f40ea26985acf77d6201dbe7a57e886b6b
xgdwq <xgdwq@126.com>
2016-09-29 16:02:09 +0800

    test first

---
+++ test_git.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+########################################################################
+# 
+# Copyright (c) 2016 Baidu.com, Inc. All Rights Reserved
+# 
+########################################################################
+ 
+"""
+File: test_git.py
+Author: wangqiang18(wangqiang18@baidu.com)
+Date: 2016/09/29 15:50:32
+"""
+print 'test_git'
+
